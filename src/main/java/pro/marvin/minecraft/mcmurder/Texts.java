7795aeeb4f4340f05f538d2dd4e7fb005e1a5ddc
Marvin Menzerath <github@marvin-menzerath.de>
2017-01-07 20:00:24 +0100

    Updated copyright

--- src/main/java/pro/marvin/minecraft/mcmurder/Texts.java
+++ src/main/java/pro/marvin/minecraft/mcmurder/Texts.java
@@ -46,6 +46,6 @@ public class Texts {
 	// End: MC-Murder
 
 	// Begin: Other
-	public static final String COPYRIGHT = "(c) 2014-2015: Marvin Menzerath - https://menzerath.eu";
+	public static final String COPYRIGHT = "(c) 2014-2017: Marvin Menzerath";
 	// End: Other
 }
