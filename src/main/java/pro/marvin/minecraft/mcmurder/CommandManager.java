5e4563b9074a0a15e98032766db1d5a38e20b641
Marvin Menzerath <github@marvin-menzerath.de>
2017-01-07 20:00:02 +0100

    Fixed indentation

--- src/main/java/pro/marvin/minecraft/mcmurder/CommandManager.java
+++ src/main/java/pro/marvin/minecraft/mcmurder/CommandManager.java
@@ -9,205 +9,205 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CommandManager implements CommandExecutor {
-    private Murder plugin;
+	private Murder plugin;
 
-    public CommandManager(Murder dta) {
-        plugin = dta;
-    }
+	public CommandManager(Murder dta) {
+		plugin = dta;
+	}
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("mcm")) {
-            // No further arguments
-            if (args.length == 0) {
-                // Send list of available commands for OPs and normal Players
-                sender.sendMessage(Texts.COMMANDS_HEAD);
-                sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_COMMANDS);
-                sender.sendMessage("§6     - mcm join [arena]");
-                sender.sendMessage("§6     - mcm leave");
-                if (sender.isOp()) {
-                    sender.sendMessage("§6     - mcm [enable/disable] [arena]");
-                    sender.sendMessage("§6     - mcm setLobby");
-                    sender.sendMessage("§6     - mcm setSpawn [arena] [spawn]");
-                    sender.sendMessage("§6     - mcm reload");
-                }
-                sender.sendMessage(Texts.COPYRIGHT);
-                return true;
-            }
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("mcm")) {
+			// No further arguments
+			if (args.length == 0) {
+				// Send list of available commands for OPs and normal Players
+				sender.sendMessage(Texts.COMMANDS_HEAD);
+				sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_COMMANDS);
+				sender.sendMessage("§6     - mcm join [arena]");
+				sender.sendMessage("§6     - mcm leave");
+				if (sender.isOp()) {
+					sender.sendMessage("§6     - mcm [enable/disable] [arena]");
+					sender.sendMessage("§6     - mcm setLobby");
+					sender.sendMessage("§6     - mcm setSpawn [arena] [spawn]");
+					sender.sendMessage("§6     - mcm reload");
+				}
+				sender.sendMessage(Texts.COPYRIGHT);
+				return true;
+			}
 
-            // One additional argument
-            if (args.length == 1) {
-                // Teleport player to lobby if he/she is not in-game
-                if (args[0].equalsIgnoreCase("lobby")) {
-                    Player player = (Player) sender;
-                    if (plugin.getPlayerInGame(player)) return true;
-                    player.teleport(plugin.mcmSpawn());
-                    return true;
-                }
+			// One additional argument
+			if (args.length == 1) {
+				// Teleport player to lobby if he/she is not in-game
+				if (args[0].equalsIgnoreCase("lobby")) {
+					Player player = (Player) sender;
+					if (plugin.getPlayerInGame(player)) return true;
+					player.teleport(plugin.mcmSpawn());
+					return true;
+				}
 
-                // Player leaves arena
-                if (args[0].equalsIgnoreCase("leave")) {
-                    Player player = (Player) sender;
-                    plugin.playerLeave(player);
-                    return true;
-                }
+				// Player leaves arena
+				if (args[0].equalsIgnoreCase("leave")) {
+					Player player = (Player) sender;
+					plugin.playerLeave(player);
+					return true;
+				}
 
-                // Set lobby-spawn [OP only]
-                if (args[0].equalsIgnoreCase("setLobby")) {
-                    if (sender.isOp()) {
-                        Player player = (Player) sender;
-                        List<Double> listPosition = Arrays.asList(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ());
-                        plugin.getConfig().set("lobbyWorld", player.getWorld().getName());
-                        plugin.getConfig().set("lobbySpawn", listPosition);
-                        plugin.saveConfig();
-                        plugin.loadConfig();
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_EXECUTED);
-                    } else {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_OP_ONLY);
-                    }
+				// Set lobby-spawn [OP only]
+				if (args[0].equalsIgnoreCase("setLobby")) {
+					if (sender.isOp()) {
+						Player player = (Player) sender;
+						List<Double> listPosition = Arrays.asList(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ());
+						plugin.getConfig().set("lobbyWorld", player.getWorld().getName());
+						plugin.getConfig().set("lobbySpawn", listPosition);
+						plugin.saveConfig();
+						plugin.loadConfig();
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_EXECUTED);
+					} else {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_OP_ONLY);
+					}
 
-                    return true;
-                }
+					return true;
+				}
 
-                // Reload config [OP only]
-                if (args[0].equalsIgnoreCase("reload")) {
-                    if (!sender.isOp()) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_OP_ONLY);
-                        return true;
-                    }
-                    plugin.loadConfig();
-                    sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_EXECUTED);
-                    return true;
-                }
-            }
+				// Reload config [OP only]
+				if (args[0].equalsIgnoreCase("reload")) {
+					if (!sender.isOp()) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_OP_ONLY);
+						return true;
+					}
+					plugin.loadConfig();
+					sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_EXECUTED);
+					return true;
+				}
+			}
 
-            // Two additional arguments
-            if (args.length == 2) {
-                // Player joins arena
-                if (args[0].equalsIgnoreCase("join") && sender instanceof Player) {
-                    Player p = (Player) sender;
+			// Two additional arguments
+			if (args.length == 2) {
+				// Player joins arena
+				if (args[0].equalsIgnoreCase("join") && sender instanceof Player) {
+					Player p = (Player) sender;
 
-                    int map;
-                    try {
-                        map = Integer.parseInt(args[1]);
-                    } catch (NumberFormatException e) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_ARENA);
-                        return true;
-                    }
+					int map;
+					try {
+						map = Integer.parseInt(args[1]);
+					} catch (NumberFormatException e) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_ARENA);
+						return true;
+					}
 
-                    if (map > 100 || map < 0 || !plugin.arenaConfig.get(map)) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INACTIVE_ARENA);
-                        return true;
-                    }
+					if (map > 100 || map < 0 || !plugin.arenaConfig.get(map)) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INACTIVE_ARENA);
+						return true;
+					}
 
-                    if (plugin.getGameStarted(map)) {
-                        p.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_GAME_RUNNING);
-                        return true;
-                    }
+					if (plugin.getGameStarted(map)) {
+						p.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_GAME_RUNNING);
+						return true;
+					}
 
-                    plugin.playerJoinedArena(p, map);
-                    return true;
-                }
+					plugin.playerJoinedArena(p, map);
+					return true;
+				}
 
-                // Enabled an arena [OP only]
-                if (args[0].equalsIgnoreCase("enable")) {
-                    if (!sender.isOp()) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_OP_ONLY);
-                        return true;
-                    }
+				// Enabled an arena [OP only]
+				if (args[0].equalsIgnoreCase("enable")) {
+					if (!sender.isOp()) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_OP_ONLY);
+						return true;
+					}
 
-                    int map;
-                    try {
-                        map = Integer.parseInt(args[1]);
-                    } catch (NumberFormatException e) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_NUMBER);
-                        return true;
-                    }
+					int map;
+					try {
+						map = Integer.parseInt(args[1]);
+					} catch (NumberFormatException e) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_NUMBER);
+						return true;
+					}
 
-                    if (map < 1 || map > 100) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_NUMBER);
-                        return true;
-                    }
+					if (map < 1 || map > 100) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_NUMBER);
+						return true;
+					}
 
-                    plugin.getConfig().set("maps." + map + ".enabled", true);
-                    plugin.saveConfig();
-                    plugin.loadConfig();
-                    sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_EXECUTED);
-                    return true;
-                }
+					plugin.getConfig().set("maps." + map + ".enabled", true);
+					plugin.saveConfig();
+					plugin.loadConfig();
+					sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_EXECUTED);
+					return true;
+				}
 
-                // Disables an arena [OP only]
-                if (args[0].equalsIgnoreCase("disable")) {
-                    if (!sender.isOp()) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_OP_ONLY);
-                        return true;
-                    }
+				// Disables an arena [OP only]
+				if (args[0].equalsIgnoreCase("disable")) {
+					if (!sender.isOp()) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_OP_ONLY);
+						return true;
+					}
 
-                    int map;
-                    try {
-                        map = Integer.parseInt(args[1]);
-                    } catch (NumberFormatException e) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_NUMBER);
-                        return true;
-                    }
+					int map;
+					try {
+						map = Integer.parseInt(args[1]);
+					} catch (NumberFormatException e) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_NUMBER);
+						return true;
+					}
 
-                    if (map < 1 || map > 100) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_NUMBER);
-                        return true;
-                    }
+					if (map < 1 || map > 100) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_NUMBER);
+						return true;
+					}
 
-                    plugin.getConfig().set("maps." + map + ".enabled", false);
-                    plugin.saveConfig();
-                    plugin.loadConfig();
-                    sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_EXECUTED);
+					plugin.getConfig().set("maps." + map + ".enabled", false);
+					plugin.saveConfig();
+					plugin.loadConfig();
+					sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_EXECUTED);
 
-                    plugin.stopGame(map, false);
-                    return true;
-                }
+					plugin.stopGame(map, false);
+					return true;
+				}
 
-                return true;
-            }
+				return true;
+			}
 
-            // Three additional arguments
-            if (args.length == 3) {
-                // Set one spawn in a map [OP only]
-                if (args[0].equalsIgnoreCase("setSpawn")) {
-                    if (!(sender instanceof Player)) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_PLAYER_ONLY);
-                        return true;
-                    }
+			// Three additional arguments
+			if (args.length == 3) {
+				// Set one spawn in a map [OP only]
+				if (args[0].equalsIgnoreCase("setSpawn")) {
+					if (!(sender instanceof Player)) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_PLAYER_ONLY);
+						return true;
+					}
 
-                    if (!sender.isOp()) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_OP_ONLY);
-                        return true;
-                    }
+					if (!sender.isOp()) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_OP_ONLY);
+						return true;
+					}
 
-                    int arg1;
-                    int arg2;
-                    try {
-                        arg1 = Integer.parseInt(args[1]);
-                        arg2 = Integer.parseInt(args[2]);
-                    } catch (NumberFormatException e) {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_NUMBER);
-                        return true;
-                    }
+					int arg1;
+					int arg2;
+					try {
+						arg1 = Integer.parseInt(args[1]);
+						arg2 = Integer.parseInt(args[2]);
+					} catch (NumberFormatException e) {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_NUMBER);
+						return true;
+					}
 
-                    if (arg1 < 100 && arg1 > 0 && arg2 < 9 && arg2 > 0) {
-                        Player player = (Player) sender;
-                        List<Double> listPosition = Arrays.asList(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ());
-                        plugin.getConfig().set("maps." + arg1 + "." + arg2, listPosition);
-                        plugin.saveConfig();
-                        plugin.loadConfig();
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_EXECUTED);
-                        return true;
-                    } else {
-                        sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_SPAWNS);
-                        return true;
-                    }
-                }
+					if (arg1 < 100 && arg1 > 0 && arg2 < 9 && arg2 > 0) {
+						Player player = (Player) sender;
+						List<Double> listPosition = Arrays.asList(player.getLocation().getX(), player.getLocation().getY(), player.getLocation().getZ());
+						plugin.getConfig().set("maps." + arg1 + "." + arg2, listPosition);
+						plugin.saveConfig();
+						plugin.loadConfig();
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_EXECUTED);
+						return true;
+					} else {
+						sender.sendMessage(Texts.PRE_TEXT + Texts.COMMANDS_INVALID_SPAWNS);
+						return true;
+					}
+				}
 
-            }
-        }
-        return false;
-    }
+			}
+		}
+		return false;
+	}
 }
--- src/main/java/pro/marvin/minecraft/mcmurder/Texts.java
+++ src/main/java/pro/marvin/minecraft/mcmurder/Texts.java
@@ -1,51 +1,51 @@
 package pro.marvin.minecraft.mcmurder;
 
 public class Texts {
-    // Begin: Main
-    public static final String PRE_TEXT = "§7[Murder] ";
-    // End: Main
+	// Begin: Main
+	public static final String PRE_TEXT = "§7[Murder] ";
+	// End: Main
 
-    // Begin: CommandManager
-    public static final String COMMANDS_HEAD = "§8-------------------=[ §6Murder §8]=-------------------";
-    public static final String COMMANDS_COMMANDS = "§6Commands:";
-    public static final String COMMANDS_EXECUTED = "§6Command successfully executed.";
-    public static final String COMMANDS_OP_ONLY = "§4Only OPs are allowed to use this command.";
-    public static final String COMMANDS_PLAYER_ONLY = "§4Only Players are allowed to use this command.";
-    public static final String COMMANDS_INVALID_ARENA = "§4This is no valid arena.";
-    public static final String COMMANDS_INVALID_NUMBER = "§4This is no valid number.";
-    public static final String COMMANDS_INACTIVE_ARENA = "§4This arena is not setup or active.";
-    public static final String COMMANDS_GAME_RUNNING = "§4This game already started. You can not join now.";
-    public static final String COMMANDS_INVALID_SPAWNS = "§4These values have to be between 1-100 (maps) and 1-8 (spawns).";
-    // End: CommandManager
+	// Begin: CommandManager
+	public static final String COMMANDS_HEAD = "§8-------------------=[ §6Murder §8]=-------------------";
+	public static final String COMMANDS_COMMANDS = "§6Commands:";
+	public static final String COMMANDS_EXECUTED = "§6Command successfully executed.";
+	public static final String COMMANDS_OP_ONLY = "§4Only OPs are allowed to use this command.";
+	public static final String COMMANDS_PLAYER_ONLY = "§4Only Players are allowed to use this command.";
+	public static final String COMMANDS_INVALID_ARENA = "§4This is no valid arena.";
+	public static final String COMMANDS_INVALID_NUMBER = "§4This is no valid number.";
+	public static final String COMMANDS_INACTIVE_ARENA = "§4This arena is not setup or active.";
+	public static final String COMMANDS_GAME_RUNNING = "§4This game already started. You can not join now.";
+	public static final String COMMANDS_INVALID_SPAWNS = "§4These values have to be between 1-100 (maps) and 1-8 (spawns).";
+	// End: CommandManager
 
-    // Begin: MC-Murder
-    public static final String GAME_PLAYER_JOINED = " §6joined the game.";
-    public static final String GAME_PLAYER_LEFT = " §6left the game.";
-    public static final String GAME_MURDERER = "§6You are the §cMURDERER§6!\nKill everyone. Don't get caught!";
-    public static final String GAME_BYSTANDER = "§6You are a §9BYSTANDER§6!\nThere is a murderer on the loose. Don't get killed!";
-    public static final String GAME_BYSTANDER_WEAPON = "§6You are a §9BYSTANDER WITH A SECRET WEAPON§6!\nThere is a murderer on the loose. Find and kill him!";
+	// Begin: MC-Murder
+	public static final String GAME_PLAYER_JOINED = " §6joined the game.";
+	public static final String GAME_PLAYER_LEFT = " §6left the game.";
+	public static final String GAME_MURDERER = "§6You are the §cMURDERER§6!\nKill everyone. Don't get caught!";
+	public static final String GAME_BYSTANDER = "§6You are a §9BYSTANDER§6!\nThere is a murderer on the loose. Don't get killed!";
+	public static final String GAME_BYSTANDER_WEAPON = "§6You are a §9BYSTANDER WITH A SECRET WEAPON§6!\nThere is a murderer on the loose. Find and kill him!";
 
-    public static final String GAME_DEATH = "§6The Murderer got you!\nYou can fly around now, but nobody is able to see you.";
-    public static final String GAME_KILLED_BYSTANDER = "§6This was not the Murderer! You should try to run...";
-    public static final String GAME_KILLED_BY_UNKNOWN = "§6What the...?! Something unknown killed you!";
-    public static final String GAME_KILLED_BY_BYSTANDER = "§6What the...?! You got killed by an other Bystander!";
-    public static final String GAME_KILLED_MURDERER = "§9%killer §6killed the Murderer!";
-    public static final String GAME_MURDERER_WINS = "§6The Murderer wins this round!";
+	public static final String GAME_DEATH = "§6The Murderer got you!\nYou can fly around now, but nobody is able to see you.";
+	public static final String GAME_KILLED_BYSTANDER = "§6This was not the Murderer! You should try to run...";
+	public static final String GAME_KILLED_BY_UNKNOWN = "§6What the...?! Something unknown killed you!";
+	public static final String GAME_KILLED_BY_BYSTANDER = "§6What the...?! You got killed by an other Bystander!";
+	public static final String GAME_KILLED_MURDERER = "§9%killer §6killed the Murderer!";
+	public static final String GAME_MURDERER_WINS = "§6The Murderer wins this round!";
 
-    public static final String GAME_HOWTO_KNIFE = "Take this powerful Knife and kill everyone!";
-    public static final String GAME_HOWTO_WEAPON = "Try to shoot the murderer with your \"great\" weapon!";
-    public static final String GAME_RELOAD_WEAPON = "§6Reload-time: ";
+	public static final String GAME_HOWTO_KNIFE = "Take this powerful Knife and kill everyone!";
+	public static final String GAME_HOWTO_WEAPON = "Try to shoot the murderer with your \"great\" weapon!";
+	public static final String GAME_RELOAD_WEAPON = "§6Reload-time: ";
 
-    public static final String GAME_ALREADY_INGAME = "§4You are already in-game. Why do you want to join again?";
-    public static final String GAME_ARENA_FULL = "$4This arena is full. Please try again later.";
+	public static final String GAME_ALREADY_INGAME = "§4You are already in-game. Why do you want to join again?";
+	public static final String GAME_ARENA_FULL = "$4This arena is full. Please try again later.";
 
-    public static final String GAME_ENDED = "§6The Game has ended. The Murderer was §c%murderer§6!";
-    public static final String GAME_STOPPED = "§6Game ended because everyone (except you) left the game.";
-    public static final String GAME_START_STOPPED = "§4Countdown stopped because you are alone in this arena now.";
-    public static final String GAME_NOT_ENOUGH_PLAYERS = "§6Countdown starts when two or more players are in-game.";
-    // End: MC-Murder
+	public static final String GAME_ENDED = "§6The Game has ended. The Murderer was §c%murderer§6!";
+	public static final String GAME_STOPPED = "§6Game ended because everyone (except you) left the game.";
+	public static final String GAME_START_STOPPED = "§4Countdown stopped because you are alone in this arena now.";
+	public static final String GAME_NOT_ENOUGH_PLAYERS = "§6Countdown starts when two or more players are in-game.";
+	// End: MC-Murder
 
-    // Begin: Other
-    public static final String COPYRIGHT = "(c) 2014-2015: Marvin Menzerath - https://menzerath.eu";
-    // End: Other
+	// Begin: Other
+	public static final String COPYRIGHT = "(c) 2014-2015: Marvin Menzerath - https://menzerath.eu";
+	// End: Other
 }
